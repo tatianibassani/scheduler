e0185b31e4cb1a964ceb0b2cf7c4146ae6aedcc7
Tatiani <tatianibassani@gmail.com>
2022-11-29 16:27:41 -0700

    Building a Buttons, add css

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,35 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+ 
+   if (props.danger) {
+     buttonClass += " button--danger";
+   }
+ 
+   return (
+     <button
+       className={buttonClass}
+       //Add an onClick and a disabled prop to the Button component.
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
+
+
+
+
+
+
+
+//export default function Button(props) {
+   //return <></>;
+//}
+
