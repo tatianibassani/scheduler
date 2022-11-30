bac59aabd8f7cf59b99e84ac5fe05078c45e4cf7
Tatiani <tatianibassani@gmail.com>
2022-11-30 14:22:22 -0700

    implementing interviwer item

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected ": props.selected
+  });
+
+  const interviewer = {
+    id: props.id,
+    name: props.name,
+    avatar: props.avatar
+    
+  };
+  
+  return(
+    <li onClick={() => props.setInterviewer(props.id)}{...props.selected} className={interviewerClass} >
+      <img
+        className="interviewers__item-image"
+        src={interviewer.avatar}
+        alt={interviewer.name}
+      />
+      {interviewer.name}
+    </li>
+  )
+};
+
+
