9072dd96ba03de54ad535aaebc0833a5e9a7f74b
Tatiani <tatianibassani@gmail.com>
2022-11-29 21:34:59 -0700

    add a function formstSpots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,37 +3,27 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const dayClass = classNames ("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected ": props.selected,
     "day-list__item--full": props.spots === 0
   });
 
+  function formatSpots(spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${spots} spots remaining`;
+    }
+    // return spots === 0 ? "no spots remaining" : spots === 1 ? "1 spot remaining" : `${spots} spots remaining`;
+  }
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
+};
 
-
-}
-  /*const dayClass = classNames ("DayListItem", {
-    "text--regular": props.name,
-    "text--light": props.spots
-
-  });
-
-  return DayListItem
-  <li
-  classNames={dayClass}
-  onClick={props.spots}
-  onClick={props.name}
-  >
-      {props.children}
-    </li>
-  );
-}*/
-
-
-
- 
