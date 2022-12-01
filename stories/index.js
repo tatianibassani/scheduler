aea9dc0955fc0f443976211626c95d2ea0fee2ac
Tatiani <tatianibassani@gmail.com>
2022-11-30 22:43:50 -0700

    implementing first appointment

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,13 +142,11 @@ storiesOf("Button", module)
           backgrounds: [{ name: "white", value: "#fff", default: true }]
         })
         .add("Appointment", () => <Appointment />)
-        .add("Appointment with Time", () => <Appointment time="12pm" />);
-              
+        .add("Appointment with Time", () => <Appointment time="12pm" />)
+        .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+
+        
         
 
-        /*.add("Clickable", () => (
-          <InterviewerList
-            interviewers={interviewers}
-            setInterviewer={action("setCurrentInterviewerID")}
-          />
-        ));*/
+        
